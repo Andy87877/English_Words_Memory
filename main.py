46394496702c6c24bbf06f45bd61d6c5a6f7bc22
Andy8787 <86054592+Andy87877@users.noreply.github.com>
2021-10-21 23:25:40 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+# https://replit.com/@andy8787main/EnglishWordsMemory
+
+''' test
+path = 'test.txt'
+f = open(path, 'w') # w寫入 a加入
+f.write('Hello World\n')
+f.write('123\n')
+f.write('123.45\n')
+f.close()
+
+# 讀取
+f = open(path)
+l = []
+for line in f.readlines():
+    l.append(line)
+print(l[0])
+f.close()
+'''
